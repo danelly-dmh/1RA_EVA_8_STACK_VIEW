9dc39ded493ea589a023ed867a94517804952c3f
danelly-dmh <danelly.dmh@gmail.com>
2017-03-04 20:04:33 -0700

    StackView

---
+++ 1RA_EVA_8_STACK_VIEW/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  1RA_EVA_8_STACK_VIEW
+//
+//  Created by TEMPORAL2 on 13/02/17.
+//  Copyright © 2017 comdanelly. All rights reserved.
+// <Hola mundo>
+// written by: DANELLY MONTAÑEZ HERNÁNDEZ
+// <INSTITUTO TECNOLÓGICO DE CHIHUAHUA II>
+// <Desarrollo de Aplicaciones para Dispositivos Móviles, Plataforma 1>
+// <03-05-2017>
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
